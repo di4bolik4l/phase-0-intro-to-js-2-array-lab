27c3d14b1e008b75c09d8b5c3e6f37486aef9b00
Justin Woodruff <justin.dwayne.woodruff@gmail.com>
2022-08-30 13:16:30 -0500

    commit tree ab249ec40039cbcc18cda5f71151d2b013dd2baa

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [
+        ...cats,
+        name
+    ];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const anotherCopyOfCats = [
+        name,
+        ...cats
+    ];
+    return anotherCopyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCatsUsingSlice = cats.slice(0, -1);
+
+    return copyOfCatsUsingSlice;
+}
+    
+function removeFirstCat() {
+    const anotherCopyOfCatsUsingSlice = cats.slice(1);
+
+    return anotherCopyOfCatsUsingSlice;
+}
+
+
+
+
+
+
+
+
